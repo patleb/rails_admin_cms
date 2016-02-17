6c984ff6837474a49301b05bac2efbe2ded44916
Patrice Lebel <patleb@users.noreply.github.com>
2016-02-17 05:28:32 -0500

    cms_locale_links added as helper

--- app/helpers/cms/locale_helper.rb
+++ app/helpers/cms/locale_helper.rb
@@ -1,21 +1,27 @@
 module CMS
   module LocaleHelper
     def cms_locale_selector
-      if RailsAdminCMS::Config.hide_current_locale?
-        links = []
-      else
-        links = [ link_to(t('cms.locale_selector.language'), '#', class: 'active') ]
-      end
-
-      I18n.available_locales.reject{ |l| l == locale }.each do |locale|
-        path = current_url_for(locale)
-        links << link_to(t('cms.locale_selector.language', locale: locale), path)
-      end
-
       content_tag(:ul, class: 'cms-locale-selector') do
-        links.each do |link|
+        cms_locale_links.each do |link|
           concat content_tag(:li, link)
         end
+      end
+    end
+
+    def cms_locale_links
+      @cms_locale_links ||= begin
+        if RailsAdminCMS::Config.hide_current_locale?
+          links = []
+        else
+          links = [ link_to(t('cms.locale_selector.language'), '#', class: 'active') ]
+        end
+
+        I18n.available_locales.reject{ |l| l == locale }.each do |locale|
+          path = current_url_for(locale)
+          links << link_to(t('cms.locale_selector.language', locale: locale), path)
+        end
+
+        links
       end
     end
 
